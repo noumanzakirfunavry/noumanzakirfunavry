6d09b89a87c7f1c4bc45d1bba47a4652e26dcf2b
Muhammad Imran <muhammad.imran@funavry.com>
2022-01-18 19:12:24 +0500

    slider

--- apps/frontend/components/Home/HorizontalMediaScrollBar.tsx/HorizontalMediaScrollBar.tsx
+++ apps/frontend/components/Home/HorizontalMediaScrollBar.tsx/HorizontalMediaScrollBar.tsx
@@ -1,10 +1,108 @@
-import { FC } from "react"
+import { FC, useState } from "react";
+import Slider from "react-slick";
+
+
 
 const HorizontalMediaScrollBar:FC = () =>{
 
+
+    
+        const [settings, setSettings] = useState({
+            dots: true,
+            infinite: true,
+            speed: 500,
+            slidesToShow: 4,
+            slidesToScroll: 4,
+            initialSlide: 0,
+            responsive: [
+              {
+                breakpoint: 1024,
+                settings: {
+                  slidesToShow: 3,
+                  slidesToScroll: 3,
+                  infinite: true,
+                  dots: true
+                }
+              },
+              {
+                breakpoint: 600,
+                settings: {
+                  slidesToShow: 2,
+                  slidesToScroll: 2,
+                  initialSlide: 2
+                }
+              },
+              {
+                breakpoint: 480,
+                settings: {
+                  slidesToShow: 1,
+                  slidesToScroll: 1
+                }
+              }
+            ]
+          });
     return (
         <>
-            Horizontal Media Scroll Bar
+        <div className="newsSlider">
+            <Slider {...settings}>
+                    <div className="slider-item">
+                        <div className="NewsBox">
+                            <div className="newscontent">
+                                <h3><a>بايدن: سيفقد حوالى 10 ملايين أميركي إعانات البطالة في حال عدم توقيع ترامب خطة التحفيز الاقتصادي </a></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="slider-item">
+                        <div className="NewsBox">
+                            <div className="newscontent">
+                                <h3><a>بايدن: سيفقد حوالى 10 ملايين أميركي إعانات البطالة في حال عدم توقيع ترامب خطة التحفيز الاقتصادي </a></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="slider-item">
+                        <div className="NewsBox">
+                            <div className="newscontent">
+                                <h3><a>بايدن: سيفقد حوالى 10 ملايين أميركي إعانات البطالة في حال عدم توقيع ترامب خطة التحفيز الاقتصادي </a></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="slider-item">
+                        <div className="NewsBox">
+                            <div className="newscontent">
+                                <h3><a>بايدن: سيفقد حوالى 10 ملايين أميركي إعانات البطالة في حال عدم توقيع ترامب خطة التحفيز الاقتصادي </a></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="slider-item">
+                        <div className="NewsBox">
+                            <div className="newscontent">
+                                <h3><a>بايدن: سيفقد حوالى 10 ملايين أميركي إعانات البطالة في حال عدم توقيع ترامب خطة التحفيز الاقتصادي </a></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="slider-item">
+                        <div className="NewsBox">
+                            <div className="newscontent">
+                                <h3><a>بايدن: سيفقد حوالى 10 ملايين أميركي إعانات البطالة في حال عدم توقيع ترامب خطة التحفيز الاقتصادي </a></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="slider-item">
+                        <div className="NewsBox">
+                            <div className="newscontent">
+                                <h3><a>بايدن: سيفقد حوالى 10 ملايين أميركي إعانات البطالة في حال عدم توقيع ترامب خطة التحفيز الاقتصادي </a></h3>
+                            </div>
+                        </div>
+                    </div>
+                    <div className="slider-item">
+                        <div className="NewsBox">
+                            <div className="newscontent">
+                                <h3><a>بايدن: سيفقد حوالى 10 ملايين أميركي إعانات البطالة في حال عدم توقيع ترامب خطة التحفيز الاقتصادي </a></h3>
+                            </div>
+                        </div>
+                    </div>
+            </Slider>
+        </div>
         </>
     )
 }
--- apps/frontend/components/Shared/owlCarousel/index.tsx
+++ apps/frontend/components/Shared/owlCarousel/index.tsx
@@ -1,5 +1,4 @@
 import React, { FC, useState } from 'react';
-import Slider from "react-slick";
 
 // function SampleNextArrow(props) {
 // const { className, style, onClick } = props;
@@ -25,70 +24,11 @@ import Slider from "react-slick";
 
 const ListCarousel: FC<any> = () => {
     
-    const [settings, setSettings] = useState({
-        dots: true,
-        infinite: true,
-        speed: 500,
-        slidesToShow: 4,
-        slidesToScroll: 4,
-        initialSlide: 0,
-        responsive: [
-          {
-            breakpoint: 1024,
-            settings: {
-              slidesToShow: 3,
-              slidesToScroll: 3,
-              infinite: true,
-              dots: true
-            }
-          },
-          {
-            breakpoint: 600,
-            settings: {
-              slidesToShow: 2,
-              slidesToScroll: 2,
-              initialSlide: 2
-            }
-          },
-          {
-            breakpoint: 480,
-            settings: {
-              slidesToShow: 1,
-              slidesToScroll: 1
-            }
-          }
-        ]
-      });
    
     
     return (
         <>
-        <Slider {...settings}>
-          <div>
-            <h3>1</h3>
-          </div>
-          <div>
-            <h3>2</h3>
-          </div>
-          <div>
-            <h3>3</h3>
-          </div>
-          <div>
-            <h3>4</h3>
-          </div>
-          <div>
-            <h3>5</h3>
-          </div>
-          <div>
-            <h3>6</h3>
-          </div>
-          <div>
-            <h3>7</h3>
-          </div>
-          <div>
-            <h3>8</h3>
-          </div>
-        </Slider>
+
         </>
     )
 }
