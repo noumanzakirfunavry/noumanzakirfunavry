65b5c57c26873823363afbace859e87c8568392e
Muhammad Imran <muhammad.imran@funavry.com>
2022-01-20 16:48:22 +0500

    tv live

--- apps/frontend/components/LiveTv/index.tsx
+++ apps/frontend/components/LiveTv/index.tsx
@@ -2,6 +2,7 @@ import HorizontalMediaScrollBar from "../Home/HorizontalMediaScrollBar.tsx/Horiz
 import AdBanner from "../Shared/AdBanner/AdBanner"
 import HorizontalFooterNewsSlider from "../Shared/HorizontalFooterNewsSlider";
 import livetvimg from "../../styles/images/livetv.png";
+import HorizontalFooter2NewsSlider from "../Shared/NewsFooter2Slider";
 
 const LiveTV = () => {
 
@@ -14,18 +15,52 @@ const LiveTV = () => {
                 <div className="pageTitle PageTitleYellow mb-0">
                     <h2>المباشر</h2>
                 </div>
-                <div className="livetvbox">
+                <div className="livetvbox mb-5">
                     <div className='liveTV'>
                         <img src={livetvimg.src} className="img-fluid"/>
                     </div>
                     <button className="btn btn-outline-light">جدول البرامج</button>
                 </div>
-                <div>
-                    Imran shb Design this slider
-                    <HorizontalFooterNewsSlider />
+                <div className="mb-3">
+                    <div className="yellowTitle">
+                        <h3>البرنامج</h3>
+                    </div>
+                    <HorizontalFooter2NewsSlider />
+                </div>
+                <div className="mb-3">
+                    <div className="yellowTitle">
+                        <h3>أحدث الحلقات</h3>
+                    </div>
+                    <HorizontalFooter2NewsSlider />
                 </div>
 
                 <AdBanner />
+
+                <div>
+                <div className="sliderTextBg mb-4">
+                    <div className="TitleBar">
+                        <h2>حصرية CNBC Arabia </h2>
+                    </div>
+                    
+                    <HorizontalFooter2NewsSlider />
+
+                </div>
+
+                </div>
+
+                <div className="mb-3">
+                    <div className="yellowTitle">
+                        <h3>أحدث مقاطع الفيديو</h3>
+                    </div>
+                    <HorizontalFooter2NewsSlider />
+                </div>
+
+                <div className="mb-3">
+                    <div className="yellowTitle">
+                        <h3>الأكثر مشاهدة</h3>
+                    </div>
+                    <HorizontalFooter2NewsSlider />
+                </div>
 
             </div>
         </>
