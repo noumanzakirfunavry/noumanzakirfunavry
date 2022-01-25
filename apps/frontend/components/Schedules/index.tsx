e9220655104b699c68510f326cf026b4c3dcd7c9
Muhammad Imran <muhammad.imran@funavry.com>
2022-01-25 11:28:20 +0500

    ui pages

--- apps/frontend/components/Schedules/index.tsx
+++ apps/frontend/components/Schedules/index.tsx
@@ -20,29 +20,115 @@ const Schedules = () => {
                             <button className="nav-link active" data-bs-toggle="pill" data-bs-target="#schedule1">اليوم<span>15 Feb</span></button>
                         </li>
                         <li className="nav-item" role="presentation">
-                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule2">السبت</button>
+                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule2">السبت<span>15 Feb</span></button>
                         </li>
                         <li className="nav-item" role="presentation">
-                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule3">الأحد</button>
+                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule3">الأحد<span>15 Feb</span></button>
                         </li>
                         <li className="nav-item" role="presentation">
-                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule4">الإثنين</button>
+                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule4">الإثنين<span>15 Feb</span></button>
                         </li>
                         <li className="nav-item" role="presentation">
-                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule5">الثلاثاء</button>
+                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule5">الثلاثاء<span>15 Feb</span></button>
                         </li>
                         <li className="nav-item" role="presentation">
-                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule6">الأربعاء</button>
+                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule6">الأربعاء<span>15 Feb</span></button>
                         </li>
                         <li className="nav-item" role="presentation">
-                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule7">الخميس</button>
+                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule7">الخميس<span>15 Feb</span></button>
                         </li>
                         <li className="nav-item" role="presentation">
-                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule8">الجمعة</button>
+                            <button className="nav-link" data-bs-toggle="pill" data-bs-target="#schedule8">الجمعة<span>15 Feb</span></button>
                         </li>
                         </ul>
                         <div className="tab-content" id="pills-tabContent">
-                            <div className="tab-pane fade show active" id="schedule1">..1.</div>
+                            <div className="tab-pane fade show active" id="schedule1">
+                                <p>KSA (GMT+3)</p>
+                                <div className="table-responsive">
+                                    <table className="table">
+                                        <tr>
+                                            <td width={200}>
+                                                <h5>08:00</h5>
+                                            </td>
+                                            <td>
+                                                <h4>اكسبو في أسبوع</h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>08:00</h5>
+                                            </td>
+                                            <td>
+                                                <h4>اكسبو في أسبوع</h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>08:00</h5>
+                                            </td>
+                                            <td>
+                                                <h4>اكسبو في أسبوع</h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>08:00</h5>
+                                            </td>
+                                            <td>
+                                                <h4>اكسبو في أسبوع</h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>08:00</h5>
+                                            </td>
+                                            <td>
+                                                <h4>اكسبو في أسبوع</h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>08:00</h5>
+                                            </td>
+                                            <td>
+                                                <h4>اكسبو في أسبوع</h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>08:00</h5>
+                                            </td>
+                                            <td>
+                                                <h4>اكسبو في أسبوع</h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>08:00</h5>
+                                            </td>
+                                            <td>
+                                                <h4>اكسبو في أسبوع</h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>08:00</h5>
+                                            </td>
+                                            <td>
+                                                <h4>اكسبو في أسبوع</h4>
+                                            </td>
+                                        </tr>
+                                        <tr>
+                                            <td>
+                                                <h5>08:00</h5>
+                                            </td>
+                                            <td>
+                                                <h4>اكسبو في أسبوع</h4>
+                                            </td>
+                                        </tr>
+                                    </table>
+                                </div>
+                            </div>
                             <div className="tab-pane fade" id="schedule2">..2.</div>
                             <div className="tab-pane fade" id="schedule3">.3..</div>
                             <div className="tab-pane fade" id="schedule4">.4..</div>
