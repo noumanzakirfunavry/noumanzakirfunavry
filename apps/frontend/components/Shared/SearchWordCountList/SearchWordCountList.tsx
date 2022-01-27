502def883ab822208d8f9a8081616fba2ee80cff
Muhammad Imran <muhammad.imran@funavry.com>
2022-01-27 19:04:10 +0500

    search page

--- apps/frontend/components/Shared/SearchWordCountList/SearchWordCountList.tsx
+++ apps/frontend/components/Shared/SearchWordCountList/SearchWordCountList.tsx
@@ -2,7 +2,62 @@ const SearchWordCountList = () => {
 
     return (
         <>
-            Search word count List Here
+            <div className="wordCountList">
+                <div className="table-responsive">
+                    <table className="table table-borderless table-striped">
+                        <tr>
+                            <td>
+                                <h5 className="m-0 p-0">AMZ</h5>
+                                Amazon Inc
+                            </td>
+                            <td className="text-start">
+                                <div className="p-0">3231.50</div>
+                                <div className="p-0 text-success">(3.09%)  99.40</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h5 className="m-0 p-0">AMZ</h5>
+                                Amazon Inc
+                            </td>
+                            <td className="text-start">
+                                <div className="p-0">3231.50</div>
+                                <div className="p-0 text-success">(3.09%)  99.40</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h5 className="m-0 p-0">AMZ</h5>
+                                Amazon Inc
+                            </td>
+                            <td className="text-start">
+                                <div className="p-0">3231.50</div>
+                                <div className="p-0 text-success">(3.09%)  99.40</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h5 className="m-0 p-0">AMZ</h5>
+                                Amazon Inc
+                            </td>
+                            <td className="text-start">
+                                <div className="p-0">3231.50</div>
+                                <div className="p-0 text-success">(3.09%)  99.40</div>
+                            </td>
+                        </tr>
+                        <tr>
+                            <td>
+                                <h5 className="m-0 p-0">AMZ</h5>
+                                Amazon Inc
+                            </td>
+                            <td className="text-start">
+                                <div className="p-0">3231.50</div>
+                                <div className="p-0 text-success">(3.09%)  99.40</div>
+                            </td>
+                        </tr>
+                    </table>
+                </div>
+            </div>
         </>
     )
 }
