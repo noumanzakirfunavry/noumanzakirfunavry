6289d94ec4ab51c5d1d594dbe7b1d6c472d35d82
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-02-04 15:23:38 +0500

    updated font

--- apps/frontend/pages/_app.tsx
+++ apps/frontend/pages/_app.tsx
@@ -27,6 +27,10 @@ function MyApp({ Component, pageProps }: AppProps) {
         <meta content="text/html; charset=UTF-8" http-equiv="content-type" />
         <meta http-equiv="content-language" content="ar" />
         <link rel="icon" type="image/x-icon" href="../favicon.ico" />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
+        <link href="https://fonts.googleapis.com/css2?family=Cairo:wght@200;300;400;500;600;700;800;900&display=swap" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css2?family=IBM+Plex+Sans+Arabic:wght@100;200;300;400;500;600;700&display=swap" rel="stylesheet" />
       </Head>
   
       <Component {...pageProps} />
