defb7a680ee690e97f0f2f4831ca48497f3beeb3
naeem <muhammad.naeem@funavry.com>
2022-03-02 17:42:41 +0500

    desktop view fixes done

--- apps/frontend/components/Shared/CategoryNews/index.tsx
+++ apps/frontend/components/Shared/CategoryNews/index.tsx
@@ -74,7 +74,7 @@ const CategoryNewsSection: FC<CategoryNewsProps> = ({limit, displayTitle, displa
                                     </div>
                             </div>
                             <div className="col-md-4 col-sm-5">
-                                    <div className="newBox">
+                                    <div className="newBox newBoxfs">
                                         <div className="NewsImage">
                                             <img className="img-fluid" src={newsImage.src} />
                                         </div>
