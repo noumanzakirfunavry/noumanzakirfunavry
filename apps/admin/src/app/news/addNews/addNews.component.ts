78745a000143abd488f04f126ef9adb14053179b
unknown <xuraiziqbal786@gmail.com>
2022-03-07 19:09:36 +0500

    apis listing completed & api service implemented

--- apps/admin/src/app/news/addNews/addNews.component.ts
+++ apps/admin/src/app/news/addNews/addNews.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit, ViewChild } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
 import { NzUploadFile } from 'ng-zorro-antd/upload';
 // import tinymce from 'tinymce/tinymce';
 
@@ -16,9 +16,10 @@ import 'tinymce/plugins/link';
 
 export class AddNewsComponent implements OnInit {
     // @ViewChild('tinyMce', { static: false }) private tinyMce;
-    previewImage: string = '';
-    previewVisible: boolean = false;
+    previewImage = '';
+    previewVisible = false;
     value: string[] = ['0-0-0'];
+
     tinyMCEConfig = {
         // base_url: 'apps/admin/src/assets/lib/tinymce/js/tinymce/',
         base_url: '/tinymce',
@@ -101,43 +102,6 @@ export class AddNewsComponent implements OnInit {
         }
     ];
 
-    // myFileBrowser (field_name, url, type, win) {
-
-    //     // alert("Field_Name: " + field_name + "nURL: " + url + "nType: " + type + "nWin: " + win); // debug/testing
-    
-    //     /* If you work with sessions in PHP and your client doesn't accept cookies you might need to carry
-    //        the session name and session ID in the request string (can look like this: "?PHPSESSID=88p0n70s9dsknra96qhuk6etm5").
-    //        These lines of code extract the necessary parameters and add them back to the filebrowser URL again. */
-    
-    //     /* Here goes the URL to your server-side script which manages all file browser things. */
-    //     var cmsURL = window.location.pathname;     // your URL could look like "/scripts/my_file_browser.php"
-    //     var searchString = window.location.search; // possible parameters
-    //     if (searchString.length < 1) {
-    //         // add "?" to the URL to include parameters (in other words: create a search string because there wasn't one before)
-    //         searchString = "?";
-    //     }
-    
-    //     // newer writing style of the TinyMCE developers for tinyMCE.openWindow
-    
-    //     this.tinyMce.openWindow({
-    //         file : cmsURL + searchString + "&type=" + type, // PHP session ID is now included if there is one at all
-    //         title : "File Browser",
-    //         width : 420,  // Your dimensions may differ - toy around with them!
-    //         height : 400,
-    //         close_previous : "no"
-    //     }, {
-    //         window : win,
-    //         input : field_name,
-    //         resizable : "yes",
-    //         inline : "yes",  // This parameter only has an effect if you use the inlinepopups plugin!
-    //         editor_id : this.tinyMce.selectedInstance.editorId
-    //     });
-    //     return false;
-    //   }
-
-    onChange($event: string[]): void {
-        console.log($event);
-    }
     editorConfig = {
         toolbar: [
             ['bold', 'italic', 'underline', 'strike'],
@@ -203,6 +167,45 @@ export class AddNewsComponent implements OnInit {
     multipleValue = ['a10', 'c12'];
     tagValue = ['a10', 'c12', 'tag'];
 
+
+    // myFileBrowser (field_name, url, type, win) {
+
+    //     // alert("Field_Name: " + field_name + "nURL: " + url + "nType: " + type + "nWin: " + win); // debug/testing
+    
+    //     /* If you work with sessions in PHP and your client doesn't accept cookies you might need to carry
+    //        the session name and session ID in the request string (can look like this: "?PHPSESSID=88p0n70s9dsknra96qhuk6etm5").
+    //        These lines of code extract the necessary parameters and add them back to the filebrowser URL again. */
+    
+    //     /* Here goes the URL to your server-side script which manages all file browser things. */
+    //     var cmsURL = window.location.pathname;     // your URL could look like "/scripts/my_file_browser.php"
+    //     var searchString = window.location.search; // possible parameters
+    //     if (searchString.length < 1) {
+    //         // add "?" to the URL to include parameters (in other words: create a search string because there wasn't one before)
+    //         searchString = "?";
+    //     }
+    
+    //     // newer writing style of the TinyMCE developers for tinyMCE.openWindow
+    
+    //     this.tinyMce.openWindow({
+    //         file : cmsURL + searchString + "&type=" + type, // PHP session ID is now included if there is one at all
+    //         title : "File Browser",
+    //         width : 420,  // Your dimensions may differ - toy around with them!
+    //         height : 400,
+    //         close_previous : "no"
+    //     }, {
+    //         window : win,
+    //         input : field_name,
+    //         resizable : "yes",
+    //         inline : "yes",  // This parameter only has an effect if you use the inlinepopups plugin!
+    //         editor_id : this.tinyMce.selectedInstance.editorId
+    //     });
+    //     return false;
+    //   }
+
+    onChange($event: string[]): void {
+        console.log($event);
+    }
+    
     ngOnInit(): void {
         const children: Array<{ label: string; value: string }> = [];
         for (let i = 10; i < 36; i++) {
@@ -217,7 +220,6 @@ export class AddNewsComponent implements OnInit {
     // }
 
     handlePreview = (file: NzUploadFile) => {
-        debugger
         this.previewImage = file.url || file.thumbUrl;
         this.previewVisible = true;
     }
--- apps/admin/src/environments/environment.ts
+++ apps/admin/src/environments/environment.ts
@@ -4,6 +4,9 @@
 
 export const environment = {
   production: false,
+  baseUrlAdmin: "http://localhost:3002/admin/api/admin/",
+  baseUrlNews: "http://localhost:3001/news/api/admin/",
+  baseUrlNewsClient: "http://localhost:3001/news/api/client/",
 };
 
 /*
