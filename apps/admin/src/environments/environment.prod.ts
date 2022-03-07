f7ebc23b420b4d1af481140435a517d6cf7d9556
unknown <xuraiziqbal786@gmail.com>
2022-03-07 19:09:36 +0500

    apis listing completed & api service implemented

--- apps/admin/src/environments/environment.prod.ts
+++ apps/admin/src/environments/environment.prod.ts
@@ -1,3 +1,6 @@
 export const environment = {
   production: true,
+  baseUrlAdmin: "http://localhost:3002/admin/api/admin/",
+  baseUrlNews: "http://localhost:3001/news/api/admin/",
+  baseUrlNewsClient: "http://localhost:3001/news/api/client/",
 };
