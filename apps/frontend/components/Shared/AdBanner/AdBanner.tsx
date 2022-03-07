febdad4d827f00a517d60442cb63630d64b3204a
naeem <muhammad.naeem@funavry.com>
2022-03-07 12:57:59 +0500

    css fixes

--- apps/frontend/components/Shared/AdBanner/AdBanner.tsx
+++ apps/frontend/components/Shared/AdBanner/AdBanner.tsx
@@ -3,7 +3,7 @@ import ad from '../../../styles/images/ad-970.jpg';
 const   AdBanner = () =>{
     return (
         <>
-            <div className="text-center mb-4 mt-4 w-75 mx-auto">
+            <div className="text-center mb-4 mt-4 w-75 w_sm_100 mx-auto">
                 <img className="img-fluid" src={ad.src} />
             </div>
         </>
