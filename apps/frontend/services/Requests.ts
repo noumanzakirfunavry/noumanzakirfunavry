f3c4cd00507f0daeaf734aea3f881e4b3a09b449
Muhammad Noman Zakir <nouman.zakir@funavry.com>
2022-03-09 21:55:22 +0500

    quick links api

--- apps/frontend/services/Requests.ts
+++ apps/frontend/services/Requests.ts
@@ -1,7 +1,8 @@
-export const baseUrl='http://52.77.29.199';
-export const newsAPI=baseUrl+'/api/admin/';
+export const baseUrl='http://157.90.67.186:3001/';
+export const newsAPI=baseUrl+'news/api/admin/';
 export const requests={
     login:newsAPI+'login',
     signup:newsAPI+'register',
     quickLinks:newsAPI+'quickLinks',
 }
+
