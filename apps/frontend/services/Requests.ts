e6efc1ab2fa630ef798423a50fbf87f85c047ccb
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-03-09 12:44:30 +0500

    updated build

---
+++ apps/frontend/services/Requests.ts
@@ -0,0 +1,7 @@
+export const baseUrl='http://52.77.29.199';
+export const newsAPI=baseUrl+'/api/admin/';
+export const requests={
+    login:newsAPI+'login',
+    signup:newsAPI+'register',
+    quickLinks:newsAPI+'quickLinks',
+}
