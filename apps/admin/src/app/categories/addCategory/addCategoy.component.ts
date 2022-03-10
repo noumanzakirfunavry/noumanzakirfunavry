5846e3508aacff28101f8441fca680c6e097d3ba
unknown <xuraiziqbal786@gmail.com>
2022-03-10 12:35:12 +0500

    category api implemented

--- apps/admin/src/app/categories/addCategory/addCategoy.component.ts
+++ apps/admin/src/app/categories/addCategory/addCategoy.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit } from '@angular/core'
 import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { ActivatedRoute, ParamMap } from '@angular/router';
+import { NzMessageService } from 'ng-zorro-antd/message';
 import { requests } from 'src/app/shared/config/config';
 import { ApiService } from 'src/app/shared/services/api.service';
 
@@ -10,9 +12,14 @@ import { ApiService } from 'src/app/shared/services/api.service';
 
 export class AddCategoryComponent implements OnInit {
     categoryForm: FormGroup;
+    categoryId: number
+    categoryById: any;
    
 
-    constructor(private fb: FormBuilder, private apiService: ApiService) {}
+    constructor(private fb: FormBuilder, 
+        private apiService: ApiService, 
+        private message: NzMessageService, 
+        private activatedRoute: ActivatedRoute) {}
 
     ngOnInit(): void {
         this.categoryForm = this.fb.group({
@@ -22,6 +29,12 @@ export class AddCategoryComponent implements OnInit {
             displayInCategoryMenu: [false],
             displayInHomePage: [false]
           });
+          this.activatedRoute.paramMap.subscribe((params: ParamMap | any) => {
+            this.categoryId = + params.get('id');
+            if (this.categoryId) {
+              this.getCategoryById();
+            }
+          });
     }
 
     categories() {
@@ -29,12 +42,28 @@ export class AddCategoryComponent implements OnInit {
             this.categoryForm.controls[i].markAsDirty();
             this.categoryForm.controls[i].updateValueAndValidity();
         }
-        const obj= this.categoryForm.value;
-        obj['parentCategoryId'] = null;
-        // obj['parentCategoryId'] = parseInt(this.categoryForm.value.parentCategoryId);
-        this.apiService.sendRequest(requests.addCategory, 'post', this.categoryForm.value).subscribe((res:any) => {
-            console.log("CATEGORIES", res);
-            this.categoryForm.reset();
+        if(this.categoryForm.valid) {
+            const obj= this.categoryForm.value;
+            obj['parentCategoryId'] = null;
+            // obj['parentCategoryId'] = parseInt(this.categoryForm.value.parentCategoryId);
+            this.apiService.sendRequest(requests.addCategory, 'post', this.categoryForm.value).subscribe((res:any) => {
+                console.log("CATEGORIES", res);
+                this.categoryForm.reset();
+            })
+        }
+    }
+
+    getCategoryById() {
+        this.apiService.sendRequest(requests.getCategoryById + this.categoryId, 'get').subscribe((res:any) => {
+            this.categoryById= res.response.category;
+            console.log("CATEGORY-BY-ID", this.categoryById);
+            this.categoryForm = this.fb.group({
+                title: [this.categoryById?.title || null, [Validators.required]],
+                parentCategoryId: [this.categoryById?.parentCategoryId || null, [Validators.required]],
+                isActive: [this.categoryById?.isActive || false],
+                displayInCategoryMenu: [this.categoryById?.displayInCategoryMenu || false],
+                displayInHomePage: [this.categoryById?.displayInHomePage || false]
+              });
         })
     }
 }    
