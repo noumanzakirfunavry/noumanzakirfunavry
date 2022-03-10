f853bba96527b38a2bf1af6072351e48dd733735
unknown <xuraiziqbal786@gmail.com>
2022-03-10 09:33:26 +0500

    working on category apis

--- apps/admin/src/app/categories/addCategory/addCategoy.component.ts
+++ apps/admin/src/app/categories/addCategory/addCategoy.component.ts
@@ -1,10 +1,40 @@
-import { Component } from '@angular/core'
+import { Component, OnInit } from '@angular/core'
+import { FormBuilder, FormGroup, Validators } from '@angular/forms'
+import { requests } from 'src/app/shared/config/config';
+import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
     selector: 'app-addCategory',
     templateUrl: './addCategory.component.html'
 })
 
-export class AddCategoryComponent {
+export class AddCategoryComponent implements OnInit {
+    categoryForm: FormGroup;
    
+
+    constructor(private fb: FormBuilder, private apiService: ApiService) {}
+
+    ngOnInit(): void {
+        this.categoryForm = this.fb.group({
+            title: [null, [Validators.required]],
+            parentCategoryId: [null, [Validators.required]],
+            isActive: [false],
+            displayInCategoryMenu: [false],
+            displayInHomePage: [false]
+          });
+    }
+
+    categories() {
+        for (const i in this.categoryForm.controls) {
+            this.categoryForm.controls[i].markAsDirty();
+            this.categoryForm.controls[i].updateValueAndValidity();
+        }
+        const obj= this.categoryForm.value;
+        obj['parentCategoryId'] = null;
+        // obj['parentCategoryId'] = parseInt(this.categoryForm.value.parentCategoryId);
+        this.apiService.sendRequest(requests.addCategory, 'post', this.categoryForm.value).subscribe((res:any) => {
+            console.log("CATEGORIES", res);
+            this.categoryForm.reset();
+        })
+    }
 }    
