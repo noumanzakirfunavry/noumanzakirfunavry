3e5c619750b03bdfac9925ec250765e0379a9af1
Hassan <hassan.javed@funavry.com>
2022-03-17 11:31:39 +0500

    sequilize connection and merge with dev-hassan

--- libs/utility/src/helpers/sequelize.connection.instance.ts
+++ libs/utility/src/helpers/sequelize.connection.instance.ts
@@ -3,7 +3,7 @@ import { Sequelize } from "sequelize-typescript";
 import 'dotenv/config'
 
 export const sequelize = new Sequelize({
-  dialect: process.env.DATABASE_TYPE as 'mysql',
+  dialect: process.env.DATABASE_TYPE as 'postgres' | 'mysql',
   host: process.env.DATABASE_HOST,
   port: parseInt(process.env.DATABASE_PORT),
   username: process.env.DATABASE_USERNAME,
