b60ba016a6daa8e2eaf7633589e324b2fb4379f2
naeem <muhammad.naeem@funavry.com>
2022-03-17 18:32:43 +0500

    css fixes for tablet view

--- apps/frontend/components/Home/NewsSection/MainSection/MainSection.tsx
+++ apps/frontend/components/Home/NewsSection/MainSection/MainSection.tsx
@@ -9,7 +9,7 @@ const MainSection:FC = () =>{
         <>
             <div className="row">
 
-                    <div className="col-lg-8">
+                    <div className="col-xl-8">
                         <div className={styles.mainNews}>
                             <div className={styles.newscontent}>
                                 <h1><a >النفط يصعد لأعلى مستوى في أسبوعين حيث أدى رفع حظر السفر الأميركي إلى زيادة الطلب</a></h1>
@@ -20,7 +20,7 @@ const MainSection:FC = () =>{
                             </div>
                         </div>
                     </div>
-                    <div className="col-lg-4">
+                    <div className="col-xl-4 d-none d-xl-block">
                         <div className="row">
                             <div className="col-sm-6 col-lg-12">
                                 <div className={styles.newsbox}>
