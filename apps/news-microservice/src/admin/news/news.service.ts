c1ff45b4450ec0ac6f7fc366acb596e15ecedc02
Hassan <hassan.javed@funavry.com>
2022-03-22 16:17:49 +0500

    seo details object added to getNewsById

--- apps/news-microservice/src/admin/news/news.service.ts
+++ apps/news-microservice/src/admin/news/news.service.ts
@@ -365,7 +365,7 @@ export class NewsService {
 
     async newsExists(id: number) {
         return await this.newsRepository.findOne({
-            include: ['tags', 'categories', 'quotes'],
+            include: ['tags', 'categories', 'quotes','seodetails'],
             where: {
                 id: id
             }
