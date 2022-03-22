eaf649380bcb1325a801624cca98e070adc15874
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-03-22 09:47:57 +0500

    working on update news

--- apps/admin/src/app/news/addNews/addNews.component.ts
+++ apps/admin/src/app/news/addNews/addNews.component.ts
@@ -5,6 +5,7 @@ import { requests } from 'src/app/shared/config/config';
 import { ApiService } from 'src/app/shared/services/api.service';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NewsModal } from 'src/app/common/models/newsModal';
+import { ActivatedRoute } from '@angular/router';
 @Component({
     selector: 'app-addNews',
     templateUrl: './addNews.component.html'
@@ -52,8 +53,11 @@ export class AddNewsComponent implements OnInit {
             comment: 'The palatable sensation we lovingly refer to as The Cheeseburger has a distinguished and illustrious history. It was born from humble roots, only to rise to well-seasoned greatness.'
         }
     ];
+    newsId: number;
 
-    constructor(private apiService: ApiService, private fb: FormBuilder) { }
+    constructor(private apiService: ApiService,
+        private fb: FormBuilder,
+        private activatedRoute: ActivatedRoute) { }
 
     ngOnInit(): void {
         this.quotesForm = this.fb.group({
@@ -62,14 +66,26 @@ export class AddNewsComponent implements OnInit {
         this.tagForm = this.fb.group({
             title: [null, [Validators.required]]
         });
-      
+
         this.newsModal = new NewsModal()
-        
-        this.initNewsForm();
+        this.activatedRoute.params.subscribe(params => {
+            this.newsId = parseInt(params.id);
+            if(!this.newsId){
+                this.initNewsForm();
+            }else{
+                this.getNews(this.newsId)
+            }
+        })
 
         this.getTags();
         this.getAllCategories();
         this.getAllQuotes()
+    }
+    getNews(newsId: number) {
+       this.apiService.sendRequest(requests.getNewsById+this.newsId,'get').subscribe(res=>{
+           console.log("news data",res);
+           
+       })
     }
     initNewsForm() {
         this.newsForm = this.fb.group({
@@ -102,19 +118,19 @@ export class AddNewsComponent implements OnInit {
             this.newsForm.controls[i].updateValueAndValidity();
         }
         // if (this.newsForm.valid) {
-            const obj = this.newsForm.value;
-            ;
-            // obj['parentCategoryId'] = parseInt(this.newsForm.value.parentCategoryId);
-            this.apiService.sendRequest(requests.addNews, 'post', this.newsModal.toServerModal(obj)).subscribe((res: any) => {
-                console.log("News", res);
-                this.newsForm.reset();
-                // if(this.categoryId) {
-                //     this.message.create('success', `Category Updated Successfully`)
-                // }
-                // else {
-                //     // this.message.create('success', `Category Added Successfully`)
-                // }
-            })
+        const obj = this.newsForm.value;
+        ;
+        // obj['parentCategoryId'] = parseInt(this.newsForm.value.parentCategoryId);
+        this.apiService.sendRequest(requests.addNews, 'post', this.newsModal.toServerModal(obj)).subscribe((res: any) => {
+            console.log("News", res);
+            this.newsForm.reset();
+            // if(this.categoryId) {
+            //     this.message.create('success', `Category Updated Successfully`)
+            // }
+            // else {
+            //     // this.message.create('success', `Category Added Successfully`)
+            // }
+        })
         // }
         console.log("form", this.newsForm.value);
 
@@ -130,7 +146,7 @@ export class AddNewsComponent implements OnInit {
     }
 
     getAllQuotes(value?) {
-        this.pagination.name=value ? value:'';
+        this.pagination.name = value ? value : '';
         this.apiService.sendRequest(requests.getAllQuotes, 'get', this.pagination).subscribe((res: any) => {
             console.log("ALL-QUOTES", res.quotes);
             this.allQuotes = res.quotes;
@@ -148,9 +164,9 @@ export class AddNewsComponent implements OnInit {
             console.log("ALL-cat", res);
             this.allCategories = res.response.categories;
             debugger
-            this.strucCategories=this.catToNodes(this.allCategories);
-            console.log("structured nodes categories=>",this.strucCategories);
-            
+            this.strucCategories = this.catToNodes(this.allCategories);
+            console.log("structured nodes categories=>", this.strucCategories);
+
         })
     }
 
@@ -168,24 +184,24 @@ export class AddNewsComponent implements OnInit {
         })
     }
 
-    catToNodes(catorgories){
-        let nodes=[];
-        this.allCategories.forEach(cat=>{
-            let parent={
-                title:cat.title,
-                value:cat.id,
-                key:cat.id,
-                children:[]
+    catToNodes(catorgories) {
+        let nodes = [];
+        this.allCategories.forEach(cat => {
+            let parent = {
+                title: cat.title,
+                value: cat.id,
+                key: cat.id,
+                children: []
             }
-            if(cat.sub){
-                parent.children=cat.sub.map(x=>{
+            if (cat.sub) {
+                parent.children = cat.sub.map(x => {
                     return {
                         title: x.title,
                         value: x.id,
                         key: x.id,
                         isLeaf: true
                     }
-                    
+
                 })
             }
             nodes.push(parent);
