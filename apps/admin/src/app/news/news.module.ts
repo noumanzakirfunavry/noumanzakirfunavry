861d713ddffdc6f923e29c3a945dd2c93128fec2
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-03-25 19:24:36 +0500

    resolved issue

--- apps/admin/src/app/news/news.module.ts
+++ apps/admin/src/app/news/news.module.ts
@@ -1,5 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from "@angular/common";
 import { SharedModule } from '../shared/shared.module';
 import { NewsRoutingModule } from "./news-routing.module";
 import { NgChartjsModule } from 'ng-chartjs';
@@ -20,7 +19,8 @@ import { NzModalModule } from 'ng-zorro-antd/modal';
 import { NzTreeSelectModule } from 'ng-zorro-antd/tree-select';
 import { CKEditorModule } from '@ckeditor/ckeditor5-angular';
 // import { FileSelectorComponent } from './file-selector/file-selector.component';
-import { FileUploaderModule } from '../modules/file-uploader/file-uploader.module';
+// import { FileUploaderModule } from '../modules/file-uploader/file-uploader.module';
+import { CommonModule } from '@angular/common';
 // import * as  DtosModule from "./../../../../../libs/dtos/src";
 const icons: IconDefinition[] = [LeftOutline, RightOutline];
 
@@ -29,8 +29,8 @@ const icons: IconDefinition[] = [LeftOutline, RightOutline];
     imports: [
         FormsModule,
         ReactiveFormsModule,
-        CommonModule,
         SharedModule,
+        CommonModule,
         NewsRoutingModule,
         NgChartjsModule,
         ...antdModule,
@@ -42,7 +42,7 @@ const icons: IconDefinition[] = [LeftOutline, RightOutline];
         NzModalModule,
         NzTreeSelectModule,
         CKEditorModule,
-        FileUploaderModule
+        // FileUploaderModule
         // DtosModule
     ],
     exports: [],
