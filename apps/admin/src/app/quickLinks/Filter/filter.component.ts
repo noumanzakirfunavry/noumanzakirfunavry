f25cd871921471945e2bf84ee30f5a4c4b3f77bb
unknown <xuraiziqbal786@gmail.com>
2022-03-29 14:11:48 +0500

    quick links pagination implemented

--- apps/admin/src/app/quickLinks/Filter/filter.component.ts
+++ apps/admin/src/app/quickLinks/Filter/filter.component.ts
@@ -1,5 +1,4 @@
 import { Component, EventEmitter, Output } from '@angular/core';
-import { ApiService } from 'src/app/shared/services/api.service';
 
 @Component({
     selector: 'quick-filter',
@@ -9,27 +8,11 @@ import { ApiService } from 'src/app/shared/services/api.service';
 export class FilterComponent {
     @Output() statusEmitter = new EventEmitter <boolean> (); 
     status: boolean;
-    // allQuickLinks: any;
-    // loading= true;
 
-
-    constructor( private apiService: ApiService ) {
-    }
-
-
-    // getAllQuickLinks() {
-    //     this.apiService.sendRequest(requests.getAllQuickLinks, 'get', this.pagination).subscribe((res:any) => {
-    //         this.allQuickLinks= res.quickLinks;
-    //         console.log("ALL-QUICK-LINKS", this.allQuickLinks);
-    //         this.loading = false;
-    //     })
-    // }
 
     onChangeStatus() {
-        // this.status= status;
         this.statusEmitter.emit(this.status);
         console.log("STATUS", this.status);
     }
-   
    
 }    
