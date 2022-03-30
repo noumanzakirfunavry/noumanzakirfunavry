00748dff2c2fa9461e26f85401a13b3c32906a00
haris-funavry <haris.ali.funavry@gmail.com>
2022-03-30 10:48:14 +0500

    generate nx library for socket module

---
+++ libs/socket/src/lib/socket.tsx
@@ -0,0 +1,12 @@
+/* eslint-disable-next-line */
+export interface SocketProps {}
+
+export function Socket(props: SocketProps) {
+  return (
+    <div>
+      <h1>Welcome to Socket!</h1>
+    </div>
+  );
+}
+
+export default Socket;
