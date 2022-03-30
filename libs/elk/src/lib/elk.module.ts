183894d19be1214a92a2310b770d0057c8ff5ccc
Hassan <hassan.javed@funavry.com>
2022-03-30 11:22:15 +0500

    module installation

--- libs/elk/src/lib/elk.module.ts
+++ libs/elk/src/lib/elk.module.ts
@@ -1,9 +1,8 @@
 import { Module } from '@nestjs/common';
 import { ElkService } from './elk.service';
-
 @Module({
   controllers: [],
   providers: [ElkService],
-  exports: [],
+  exports: []
 })
 export class ElkModule {}
