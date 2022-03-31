d8240365c71fe0a099db004e7b53972c8a5f45eb
haris-funavry <haris.ali.funavry@gmail.com>
2022-03-31 12:17:44 +0500

    Implement Create Menu API

--- apps/admin-microservice/src/app/menus/menus.controller.ts
+++ apps/admin-microservice/src/app/menus/menus.controller.ts
@@ -1,7 +1,13 @@
-import { Controller } from '@nestjs/common';
+import { CreateMenuRequestDto } from '@cnbc-monorepo/dtos';
+import { Body, Controller, Post, Req } from '@nestjs/common';
 import { MenusService } from './menus.service';
 
-@Controller('menus')
+@Controller('admin/api/admin/menus')
 export class MenusController {
   constructor(private readonly menusService: MenusService) {}
+
+	@Post()
+	createMenu(@Body() createMenuRequestDto: CreateMenuRequestDto, @Req() req){		
+		return this.menusService.createMenu(createMenuRequestDto, req.user.data.id);
+	}
 }
