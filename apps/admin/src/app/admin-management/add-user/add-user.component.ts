9b14526db9100fd4a207a31d9ddbf2968aad0e6a
unknown <xuraiziqbal786@gmail.com>
2022-04-04 15:05:24 +0500

    jobs api implemented & validation applied on name and username in admin

--- apps/admin/src/app/admin-management/add-user/add-user.component.ts
+++ apps/admin/src/app/admin-management/add-user/add-user.component.ts
@@ -60,9 +60,11 @@ export class AddUserComponent implements OnInit{
 
   inItForm() {
     this.adminForm = this.fb.group({
-      name: [null, [Validators.required, Validators.pattern('[A-Za-z ]*$'), Validators.minLength(3), Validators.maxLength(250)]],
+      // name: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(250), Validators.pattern('[A-Za-z ]*$')]],
+      name: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(250), Validators.pattern('^(?:[\u0009-\u000D\u001C-\u007E\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDCF\uFDF0-\uFDFF\uFE70-\uFEFF]|(?:\uD802[\uDE60-\uDE9F]|\uD83B[\uDE00-\uDEFF])){0,250}$')]],
       rolesId: [null, [Validators.required]],
-      userName: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(250), Validators.pattern('[a-zA-Z0-9_-]*$')]],
+      // userName: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(250), Validators.pattern('^[A-Za-z][A-Za-z0-9_][A-Za-z0-9!@#$%^&*_]{0,250}$')]],
+      userName: [null, [Validators.required, Validators.minLength(3), Validators.maxLength(250), Validators.pattern('^(?:[a-zA-Z0-9\s!@,=%$#&*_\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDCF\uFDF0-\uFDFF\uFE70-\uFEFF]|(?:\uD802[\uDE60-\uDE9F]|\uD83B[\uDE00-\uDEFF])){0,250}$')]],
       password: [null, [Validators.required, Validators.minLength(6), Validators.maxLength(30)]],
       confirmPassword: [null, [Validators.required, this.confirmationValidator]],
       email: [null, [Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,5}$'), Validators.required]],
@@ -117,9 +119,9 @@ export class AddUserComponent implements OnInit{
       this.userById= res.response.admin;
       console.log("USER-BY-ID", this.userById);
       this.adminForm = this.fb.group({
-        name: [this.userById?.name || null, [Validators.required, Validators.pattern('[A-Za-z ]*$'), Validators.minLength(3), Validators.maxLength(250)]],
+        name: [this.userById?.name || null, [Validators.required, Validators.minLength(3), Validators.maxLength(250), Validators.pattern('^(?:[\u0009-\u000D\u001C-\u007E\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDCF\uFDF0-\uFDFF\uFE70-\uFEFF]|(?:\uD802[\uDE60-\uDE9F]|\uD83B[\uDE00-\uDEFF])){0,250}$')]],
         rolesId: [this.userById?.rolesId || null, [Validators.required]],
-        userName: [this.userById?.userName || null, [Validators.required, Validators.minLength(3), Validators.maxLength(250), Validators.pattern('[a-zA-Z0-9_-]*$')]],
+        userName: [this.userById?.userName || null, [Validators.required, Validators.minLength(3), Validators.maxLength(250), Validators.pattern('^(?:[a-zA-Z0-9\s!@,=%$#&*_\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDCF\uFDF0-\uFDFF\uFE70-\uFEFF]|(?:\uD802[\uDE60-\uDE9F]|\uD83B[\uDE00-\uDEFF])){0,250}$')]],
         password: [this.userById?.password || null, [Validators.required, Validators.minLength(6), Validators.maxLength(30)]],
         confirmPassword: [this.userById?.password || null, [Validators.required, this.confirmationValidator]],
         email: [this.userById?.email || null, [Validators.pattern('^[a-z0-9._%+-]+@[a-z0-9.-]+\\.[a-z]{2,5}$'), Validators.required]],
--- apps/admin/src/app/news/addNews/addNews.component.ts
+++ apps/admin/src/app/news/addNews/addNews.component.ts
@@ -108,7 +108,7 @@ export class AddNewsComponent implements OnInit {
 
     private initQuoteForm() {
         this.quotesForm = this.fb.group({
-            name: [null, [Validators.required]]
+            name: [null, [Validators.required, Validators.pattern('^(?:[a-zA-Z0-9\s!@,=%$#&*_\u0600-\u06FF\u0750-\u077F\u08A0-\u08FF\uFB50-\uFDCF\uFDF0-\uFDFF\uFE70-\uFEFF]|(?:\uD802[\uDE60-\uDE9F]|\uD83B[\uDE00-\uDEFF])){0,250}$')]]
         });
     }
 
@@ -276,22 +276,31 @@ export class AddNewsComponent implements OnInit {
 
     getAllQuotes(value?) {
         this.pagination.name = value ? value : '';
-        this.apiService.sendRequest(requests.getAllQuotes, 'get', this.pagination).subscribe((res: any) => {
+        this.apiService.sendRequest(requests.getAllQuotes, 'get', this.clean(Object.assign({...this.pagination}))).subscribe((res: any) => {
             console.log("ALL-QUOTES", res.quotes);
             this.allQuotes = res.quotes;
         })
     }
 
+    clean(obj:any) {
+        for (const propName in obj) {
+          if (obj[propName] === null || obj[propName] === undefined || obj[propName] === "" || obj[propName] === []) {
+            delete obj[propName];
+          }
+        }
+        return obj
+    }
+
     getTags(value?) {
         this.pagination.title = value ? value : '';
-        this.apiService.sendRequest(requests.getAllTags, 'get', this.pagination).subscribe((res: any) => {
+        this.apiService.sendRequest(requests.getAllTags, 'get', this.clean(Object.assign({...this.pagination}))).subscribe((res: any) => {
             console.log("ALL-tags", res.response.tags);
             this.allTags = res.response.tags;
         })
     }
 
     getAllCategories() {
-        this.apiService.sendRequest(requests.getAllCategories, 'get', this.pagination).subscribe((res: any) => {
+        this.apiService.sendRequest(requests.getAllCategories, 'get', this.clean(Object.assign({...this.pagination}))).subscribe((res: any) => {
             console.log("ALL-cat", res);
             this.allCategories = res.response.categories;
             this.strucCategories = this.catToNodes(this.allCategories);
@@ -301,12 +310,18 @@ export class AddNewsComponent implements OnInit {
     }
 
     addNewQuote(value?) {
-        this.apiService.sendRequest(requests.addNewQuote, 'post', this.quotesForm.value).subscribe((res: any) => {
-            this.allQuotes = res.quote;
-            this.initQuoteForm();
-            this.getAllQuotes();
-            console.log("ADD-TAG", this.allQuotes);
-        })
+        for (const i in this.quotesForm.controls) {
+            this.quotesForm.controls[i].markAsDirty();
+            this.quotesForm.controls[i].updateValueAndValidity();
+        }
+        if(this.quotesForm.valid) {
+            this.apiService.sendRequest(requests.addNewQuote, 'post', this.quotesForm.value).subscribe((res: any) => {
+                this.allQuotes = res.quote;
+                this.initQuoteForm();
+                this.getAllQuotes();
+                console.log("ADD-TAG", this.allQuotes);
+            })
+        }
     }
 
     addNewTag() {
