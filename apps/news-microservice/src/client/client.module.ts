eb951a0eaa2c3a77fe870de7b0917d0092ba957b
haris-funavry <haris.ali.funavry@gmail.com>
2022-04-06 12:12:29 +0500

    Implement client get news by Id

--- apps/news-microservice/src/client/client.module.ts
+++ apps/news-microservice/src/client/client.module.ts
@@ -3,13 +3,15 @@ import { QuickLinksModule } from "./quickLinks/quickLinks.module";
 import { QuotesModule } from "./quotes/quotes.module";
 import { TagsModule } from "./tags/tags.module";
 import { NewsTypesModule } from './news-types/news-types.module';
+import { NewsModule } from './news/news.module';
 
 @Module({
     imports:[
         TagsModule,
         QuickLinksModule,
         QuotesModule,
-        NewsTypesModule
+        NewsTypesModule,
+        NewsModule
     ],
 })
 export class  ClientModule{}
