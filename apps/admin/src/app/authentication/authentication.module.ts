9de965f3e9dae87cdf5f3477a8ee5d359874f223
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-04-08 12:39:40 +0500

    updated cathca

--- apps/admin/src/app/authentication/authentication.module.ts
+++ apps/admin/src/app/authentication/authentication.module.ts
@@ -14,6 +14,7 @@ import { Error2Component } from './error-2/error-2.component';
 import { RouterModule } from '@angular/router';
 import { ResetPasswordComponent } from './reset-password/reset-password.component';
 import { SetPasswordComponent } from './set-password/set-password.component';
+import { NgxCaptchaModule } from 'ngx-captcha';
 
 const antdModule= [
     NzFormModule,
@@ -30,7 +31,8 @@ const antdModule= [
         ReactiveFormsModule,
         AuthenticationRoutingModule,
         RouterModule,
-        ...antdModule
+        ...antdModule,
+        NgxCaptchaModule
     ],
     declarations: [
         LoginComponent,
