a2ba42c82c7eb7cf38b0edf7e9df3bc69ffbf4d2
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-04-11 01:31:16 +0500

    updated cat news

--- apps/frontend/pages/index.tsx
+++ apps/frontend/pages/index.tsx
@@ -171,11 +171,37 @@ export function Index() {
         </div>
       </div>
 
+      {
+    !categories?.length && <Skeleton/>
+  }
+  {
+    categories?.length && categories?.map((category:CategoryProps, index:number)=>{
+      return (
+        <div className='PageBuilder-pageRow' key={category.id}>
+          <div className='PageBuilder-col-9'>
+            <CategoryNewsSection cat={category} limit = {1} displayTitle={true} displayTopTwoNews={true} displayMoreButton={false}/>
+          </div>
+          <div className='PageBuilder-sidebar'>
+                {
+                  index === 0 && (
+                    <SideBar sideBarSequence={[{ componentType: 'simple', position: 1 , title:'الأكثر قراءة'}, /*{ componentType: 'dotList', position: 2 }*/ ]} />
+                  )
+                }
+                {
+                  index === 2 && (
+                    <SideBar sideBarSequence={[{ componentType: 'LargeBanner', position: 2 }]} />
+                  )
+                }
+          </div>
+        </div>
+      )
+    })
+  }
 {/* categoreis code is save on usman's side i will revert it after integrating it */}
 
-      {/* <div className="text-center mt-3 mb-4 more_btn" onClick={()=>setLimit(limit + 5)}>
+      <div className="text-center mt-3 mb-4 more_btn" onClick={()=>setLimit(limit + 5)}>
           <button className="btn btn-outline-primary">المزيد</button>
-      </div> */}
+      </div>
 
       {/* </div> */}
 
