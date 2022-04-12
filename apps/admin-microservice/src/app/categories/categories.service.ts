ca3f03b26925f0f5389f6127a50b6ea044cbfdcf
haris-funavry <haris.ali.funavry@gmail.com>
2022-04-12 14:07:20 +0500

    Fix Categories GetAll, GetById

--- apps/admin-microservice/src/app/categories/categories.service.ts
+++ apps/admin-microservice/src/app/categories/categories.service.ts
@@ -13,7 +13,7 @@ export class CategoriesService {
     ) { }
 
     async getById(id: number) {
-        const result = await this.categoryRepo.findOne({ where: { id: id } })
+        const result = await this.categoryRepo.findOne({ where: { id: id }, include: [{ model: Categories, as: 'sub' }], })
         if (!result) {
             throw new CustomException(
                 Exceptions[ExceptionType.RECORD_NOT_FOUND].message,
@@ -85,34 +85,22 @@ export class CategoriesService {
     }
 
     async getAll(query: GetAllCategoriesRequestDto) {
+				let { limit, pageNo, ...where } = query
         let offset = 0
-        query.pageNo = query.pageNo - 1;
-        if (query.pageNo) offset = query.limit * query.pageNo;
-        let where = {}
-        if (query.status) {
-            where['isActive'] = JSON.parse(query.status.toString())
-        }
-        if (query.parentCategoryId) {
-            where['parentCategoryId'] = query.parentCategoryId
-        }
-        if (query.title) {
-            where['title'] = query.title
-        }
-        if (query.publishers) {
-            where['publishedBy'] = query.publishers
-        }
-        if (query.includeNews) { //TODO
-
-        }
-        if (query.includeNews) {//TODO
-
-        }
+        pageNo = pageNo - 1;
+        if (pageNo) offset = limit * pageNo;
+        
         let result = await this.categoryRepo.findAndCountAll(
 
             {
-                include: ['user'],
-                where: where,
-                limit: query.limit, offset: offset
+                include: ['user', { model: Categories, as: 'sub', where: {
+									displayInHomePage: query.displayInHomePage || false, 
+									displayInCategoryMenu: query.displayInCategoryMenu || true,
+									isActive: true
+								}}],
+                where: {...where, isActive: true},
+                limit, 
+								offset
             }
         )
         if (!result.count) {
@@ -121,23 +109,25 @@ export class CategoriesService {
                 Exceptions[ExceptionType.RECORD_NOT_FOUND].status
             )
         }
-        result.rows = result.rows.map(item => item.toJSON())
+				// ** Recursion has been implemented for future so that nested submenus can be fetched if there is requirement,
+				// ** For example, populating main menus with sub menus, with each sub menu having more sub menus etc */ 
+        // result.rows = result.rows.map(item => item.toJSON())
 
-        let categories = result.rows.filter((item) => item.parentCategoryId == null)
+        // let categories = result.rows.filter((item) => item.parentCategoryId == null)
 
-        // Removing The Top Level Categories from the original result
-        for (let index = 0; index < categories.length; index++) {
-            const element = categories[index];
-            result.rows = this.removeItemOnce(result.rows, element);
-        }
-        // Now Calling to fit all remaining categories
-        this.makingNested(result.rows, categories, 0)
+        // // Removing The Top Level Categories from the original result
+        // for (let index = 0; index < categories.length; index++) {
+        //     const element = categories[index];
+        //     result.rows = this.removeItemOnce(result.rows, element);
+        // }
+        // // Now Calling to fit all remaining categories
+        // this.makingNested(result.rows, categories, 0)
 
         return new GenericResponseDto(
             HttpStatus.OK,
             "FETCHED SUCCESSFULLY",
             {
-                categories: categories,
+                categories: result.rows,
                 totalCount: await this.parentCountQuery()
             }
         );
