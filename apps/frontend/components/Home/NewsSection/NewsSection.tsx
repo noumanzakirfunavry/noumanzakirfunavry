3cc78db953423a2bdfc1303d9e128f6808ea3b04
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-04-13 18:11:55 +0500

    updated images

--- apps/frontend/components/Home/NewsSection/NewsSection.tsx
+++ apps/frontend/components/Home/NewsSection/NewsSection.tsx
@@ -25,13 +25,13 @@ const NewsSection:FC = () =>{
     },[])
 
     const getDataFromApi = () =>{
-        GetData(`${requests.featuredNews}&limit=10&pageNo=1`,{},"get", false).then((res)=>{
+        GetData(`${requests.featuredNews}&limit=8&pageNo=1`,{},"get", false).then((res)=>{
             featuredNews.mainNews = res.data?.filter((news:any)=>{
-                return news._source.featuredNews 
+                return news._source.featuredNews?.section=='MAIN' 
             })
 
             featuredNews.secondaryNews = res.data?.filter((news:any)=>{
-                return !news._source.featuredNews 
+                return news._source.featuredNews?.section!='MAIN'
             })
             setFeaturedNews({...featuredNews})
         }).catch(err=>{
