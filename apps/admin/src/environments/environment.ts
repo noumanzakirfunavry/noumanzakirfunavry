a42c6b29472dd6162bf4c854644025797ab56a1b
unknown <xuraiziqbal786@gmail.com>
2022-04-17 16:22:15 +0500

    category field updated in add news

--- apps/admin/src/environments/environment.ts
+++ apps/admin/src/environments/environment.ts
@@ -10,17 +10,17 @@ export const environment = {
   // baseUrlAdminClient: "https://backend.admin.staging.cnbcarabia.com/admin/api/client/",
   //  fileUrl:"https://backend.admin.staging.cnbcarabia.com"
   //  client
-  // baseUrlAdmin: "http://157.90.67.186:3002/admin/api/admin/",
-  // baseUrlNews: "http://157.90.67.186:3001/news/api/admin/",
-  // baseUrlNewsClient: "http://157.90.67.186:3001/news/api/client/",
-  // baseUrlAdminClient: "http://157.90.67.186:3002/admin/api/client/",
-  //  fileUrl:"http://157.90.67.186:3002"
+  baseUrlAdmin: "http://157.90.67.186:3002/admin/api/admin/",
+  baseUrlNews: "http://157.90.67.186:3001/news/api/admin/",
+  baseUrlNewsClient: "http://157.90.67.186:3001/news/api/client/",
+  baseUrlAdminClient: "http://157.90.67.186:3002/admin/api/client/",
+   fileUrl:"http://157.90.67.186:3002"
   // QA
-  baseUrlAdmin: "http://157.90.67.186:4002/admin/api/admin/",
-  baseUrlNews: "http://157.90.67.186:4001/news/api/admin/",
-  baseUrlNewsClient: "http://157.90.67.186:4001/news/api/client/",
-  baseUrlAdminClient: "http://157.90.67.186:4002/admin/api/client/",
-  fileUrl:"http://157.90.67.186:4002/"
+  // baseUrlAdmin: "http://157.90.67.186:4002/admin/api/admin/",
+  // baseUrlNews: "http://157.90.67.186:4001/news/api/admin/",
+  // baseUrlNewsClient: "http://157.90.67.186:4001/news/api/client/",
+  // baseUrlAdminClient: "http://157.90.67.186:4002/admin/api/client/",
+  // fileUrl:"http://157.90.67.186:4002/"
 };
 
 /*
