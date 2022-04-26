70acb08f79ab7e52e886620c77e7afade67b41e8
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-04-26 17:33:27 +0500

    ipdatec free version tiny

--- apps/admin/src/app/news/addNews/addNews.component.ts
+++ apps/admin/src/app/news/addNews/addNews.component.ts
@@ -66,36 +66,35 @@ export class AddNewsComponent implements OnInit {
             apiKey:"pl277auj2y5uqk3nkk28sz4d32vimlj6ezd5b6t6vee325u4",
             base_url: '/tinymce',
             suffix: '.min',
-            'plugins': [
-                'code print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons'
+            'plugins': 
+            'preview importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap pagebreak nonbreaking anchor insertdatetime advlist lists wordcount help charmap quickbars emoticons',
+
+                // 'code print preview paste importcss searchreplace autolink autosave save directionality code visualblocks visualchars fullscreen image link media template codesample table charmap hr pagebreak nonbreaking anchor toc insertdatetime advlist lists wordcount imagetools textpattern noneditable help charmap quickbars emoticons'
                 // "advlist autolink link image lists charmap print preview hr anchor pagebreak",
                 // "searchreplace wordcount visualblocks visualchars insertdatetime media nonbreaking",
                 // "table contextmenu directionality emoticons paste textcolor responsivefilemanager code",
                 // "advlist autolink lists link image charmap print preview anchor",
                 // "searchreplace visualblocks code fullscreen",
                 // "insertdatetime media table contextmenu paste qrcode youtube twitter"
-            ],
+            
             directionality : 'rtl',
-            toolbar: 'undo redo | code bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist checklist | forecolor backcolor casechange permanentpen formatpainter removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media pageembed template link anchor codesample | a11ycheck ltr rtl | showcomments addcomment',
-            // toolbar: 'undo redo | bold italic underline strikethrough | fontselect fontsizeselect formatselect | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
-            'toolbar1' : "undo redo | bold italic underline | alignleft aligncenter alignright alignjustify | bullist numlist outdent indent | styleselect | fontselect | fontsizeselect",
-            'toolbar2' : "youtube twitter | responsivefilemanager | link image qrcode | link unlink anchor | image media | forecolor backcolor  | print preview code ",
+            menubar: 'file edit view insert format custom tools table help',
+            toolbar: 'undo redo | bold italic underline strikethrough | fontfamily fontsize blocks | alignleft aligncenter alignright alignjustify | outdent indent |  numlist bullist | forecolor backcolor removeformat | pagebreak | charmap emoticons | fullscreen  preview save print | insertfile image media template link anchor codesample | ltr rtl',
             'image_advtab': true,
             menu: {
-                custom: { title: 'Custom File Manager', items: 'myCustomMenuItem' }
+                custom: { title: 'Custom', items: 'myCustomMenuItem' }
             },
-            menubar: 'file edit view insert table custom format tools',
+            // menubar: 'file edit view insert table custom format tools',
             setup: function (editor) {
                 let self = selfp;
                 editor.ui.registry.addMenuItem('myCustomMenuItem', {
                     text: 'Upload',
                     onAction:
-                        (function () {
-                            // self.$isVisible.next(true)
-                            // alert('Menu item clicked' + self.isVisible);
+                        (function () {                          
                             self.toggleModal();
                         }).bind(this)
                 })
+                
             },
             images_upload_url: requests.addNewAttachment,
             automatic_uploads: true,
