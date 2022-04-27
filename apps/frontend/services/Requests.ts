a02171b675354d5e5a4ad0fa11404b92e66fe5ae
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-04-27 15:51:27 +0500

    updating zagtrader

--- apps/frontend/services/Requests.ts
+++ apps/frontend/services/Requests.ts
@@ -1,9 +1,9 @@
 
-// export const baseUrl='http://157.90.67.186:3001/';
-// export const baseUrlAdmin='http://157.90.67.186:3002/';
+export const baseUrl='http://157.90.67.186:3001/';
+export const baseUrlAdmin='http://157.90.67.186:3002/';
 // QA
-export const baseUrl='http://157.90.67.186:4001/';
-export const baseUrlAdmin='http://157.90.67.186:4002/';
+// export const baseUrl='http://157.90.67.186:4001/';
+// export const baseUrlAdmin='http://157.90.67.186:4002/';
 // domains
 // export const baseUrlAdmin= "https://backend.admin.staging.cnbcarabia.com/";
 // export const baseUrl= "https://backend.news.staging.cnbcarabia.com/";
