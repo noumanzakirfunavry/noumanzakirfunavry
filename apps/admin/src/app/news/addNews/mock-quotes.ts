7c9366878eed1717004b1aaf6806f8814882c247
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-04-29 15:31:13 +0500

    updated quotes symbols moc

---
+++ apps/admin/src/app/news/addNews/mock-quotes.ts
@@ -0,0 +1,43 @@
+export const QuotesMockData=
+[
+    {
+        quoteSymbol:'GOLDENSON',
+        quoteTitle:'GOLDEN SON LIMITED',
+        quoteTickerId:329877
+    },
+    {
+        quoteSymbol:'GOLD',
+        quoteTitle:'DUBAI GOLD SECURITIES',
+        quoteTickerId:316750
+    },
+    {
+        quoteSymbol:'GOK',
+        quoteTitle:'GEOKINETICS INC. COMMON STOCK',
+        quoteTickerId:313137
+    },
+    {
+        quoteSymbol:'GOIL',
+        quoteTitle:'GHANA OIL COMPANY',
+        quoteTickerId:316852
+    },
+    {
+        quoteSymbol:'GOGO',
+        quoteTitle:'GOGO INC',
+        quoteTickerId:321946
+    },
+    {
+        quoteSymbol:'GOGL',
+        quoteTitle:'GOLDEN OCEAN GROUP LIMITED',
+        quoteTickerId:323690
+    },
+    {
+        quoteSymbol:'GOF',
+        quoteTitle:'GUGGENHEIM STRATEGIC OPPORTUNITIES FUND COMMON SHARES OF BENEFICIAL INTEREST',
+        quoteTickerId:314750
+    },
+    {
+        quoteSymbol:'GOCO.CA',
+        quoteTitle:'Golden Coast Company',
+        quoteTickerId:168641
+    },
+]
