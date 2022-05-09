93b43a467ed9d7ac5fe2f5456a7a394d944db5b9
Muhammad Noman Zakir <nouman.zakir@funavry.com>
2022-05-09 15:51:02 +0500

    sitemap test

--- apps/frontend/next-sitemap.js
+++ apps/frontend/next-sitemap.js
@@ -3,4 +3,10 @@ const siteUrl = "https://site-staging.cnbcarabia.com/"
 module.exports = {
     siteUrl,
     generateRobotsTxt : true,
+    exclude: ['/sitemap.xml'], // <= exclude here
+    robotsTxtOptions: {
+        additionalSitemaps: [
+        'https://site-staging.cnbcarabia.com/sitemap.xml', // <==== Add here
+        ],
+    },
 }
