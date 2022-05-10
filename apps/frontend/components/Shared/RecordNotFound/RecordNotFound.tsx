bed2a8154b21c40791a9b62b8b496ec165c26dc2
Maryam <maryam.naseem@funavry.com>
2022-05-10 17:01:52 +0500

    programs: record not found component

---
+++ apps/frontend/components/Shared/RecordNotFound/RecordNotFound.tsx
@@ -0,0 +1,12 @@
+
+const   RecordNotFound = () =>{
+    return (
+        <>
+            <div className="text-center mb-4 mt-4 w_sm_100 mx-auto">
+                <h5>This record could not be found</h5>
+            </div>
+        </>
+    )
+}
+
+export default RecordNotFound
