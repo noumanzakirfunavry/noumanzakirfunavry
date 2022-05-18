f919497dd225720e7d9601a1d381a698be8249ee
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-05-18 14:10:57 +0500

    updated image path for domain

--- apps/frontend/services/StaticData.tsx
+++ apps/frontend/services/StaticData.tsx
@@ -1,23 +1,25 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
 import axios from 'axios'
+import { baseUrlAdmin, requests } from './Requests';
 
 export const GetMetaData = (seoData?:any) => {
 
     const data = {
       description:seoData?.description || "CNBC Meta Tag Description",
       title:seoData?.title || "CNBC",
-      image:seoData?.image?.url || "icon",
+      image:baseUrlAdmin+seoData?.image?.path || "icon",
       siteName:"CNBC Arabia",
       keywords:seoData?.keywords || "cnbc meta tag keywords",
       og:{
         description:seoData?.description || "CNBC Meta Tag og: description",
         title:seoData?.title || "og title cnbc",
-        image:seoData?.image?.url || "icon",
+        image:baseUrlAdmin+seoData?.image?.path || "icon",
       },
       twitter:{
         description:seoData?.description || "CNBC Meta Tag twitter: description",
         title:seoData?.description || "CNBC",
-        image:seoData?.image?.url || "icon",
+        // image:seoData?.image?.url || "icon",
+        image:baseUrlAdmin+seoData?.image?.path || "icon",
         site:"@CNBCArabia",
         card:seoData?.image?.title || "summary_large_image"
       }
