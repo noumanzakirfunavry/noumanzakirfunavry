97fe639021201ae61abc01688d5adf2739386c90
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-05-23 19:02:43 +0500

    updated seo image

--- apps/frontend/services/StaticData.tsx
+++ apps/frontend/services/StaticData.tsx
@@ -8,18 +8,18 @@ export const GetMetaData = (seoData?:any) => {
     const data = {
       description:seoData?.description || "CNBC Meta Tag Description",
       title:seoData?.title || "CNBC",
-      image:seoData?.image?.path ? baseUrlAdmin+seoData?.image?.path:logoImage.src,
+      image:seoData?.image?.path ? baseUrlAdmin+seoData?.image?.path:'https://site-staging.cnbcarabia.com'+logoImage.src,
       siteName:"CNBC Arabia",
       keywords:seoData?.keywords || "cnbc meta tag keywords",
       og:{
         description:seoData?.description || "CNBC Meta Tag og: description",
         title:seoData?.title || "og title cnbc",
-        image:seoData?.image?.path ? baseUrlAdmin+seoData?.image?.path : logoImage.src,
+        image:seoData?.image?.path ? baseUrlAdmin+seoData?.image?.path :'https://site-staging.cnbcarabia.com' +logoImage.src,
       },
       twitter:{
         description:seoData?.description || "CNBC Meta Tag twitter: description",
         title:seoData?.description || "CNBC Arabia",
-        image:seoData?.image?.path ? baseUrlAdmin+seoData?.image?.path: logoImage.src,
+        image:seoData?.image?.path ? baseUrlAdmin+seoData?.image?.path:'https://site-staging.cnbcarabia.com'+ logoImage.src,
         site:"@CNBCArabia",
         card:"summary_large_image"
       }
