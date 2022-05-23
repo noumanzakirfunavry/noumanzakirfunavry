c3b433cf510fac0b714a5d3b378b7437e9894324
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-05-23 18:46:54 +0500

    updated seo image tags

--- apps/frontend/services/StaticData.tsx
+++ apps/frontend/services/StaticData.tsx
@@ -1,25 +1,27 @@
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import axios from 'axios'
+// import axios from 'axios'
+import { baseUrlAdmin } from './Requests';
+import  logoImage  from "../styles/images/CNBC-favicon.png";
 
 export const GetMetaData = (seoData?:any) => {
 
     const data = {
       description:seoData?.description || "CNBC Meta Tag Description",
       title:seoData?.title || "CNBC",
-      image:seoData?.image?.url || "icon",
+      image:seoData?.image?.path ? baseUrlAdmin+seoData?.image?.path:logoImage.src,
       siteName:"CNBC Arabia",
       keywords:seoData?.keywords || "cnbc meta tag keywords",
       og:{
         description:seoData?.description || "CNBC Meta Tag og: description",
         title:seoData?.title || "og title cnbc",
-        image:seoData?.image?.url || "icon",
+        image:seoData?.image?.path ? baseUrlAdmin+seoData?.image?.path : logoImage.src,
       },
       twitter:{
         description:seoData?.description || "CNBC Meta Tag twitter: description",
-        title:seoData?.description || "CNBC",
-        image:seoData?.image?.url || "icon",
+        title:seoData?.description || "CNBC Arabia",
+        image:seoData?.image?.path ? baseUrlAdmin+seoData?.image?.path: logoImage.src,
         site:"@CNBCArabia",
-        card:seoData?.image?.title || "summary_large_image"
+        card:"summary_large_image"
       }
     }
 
