3164ae0927bbf6d9e6292fe16927d951f9a79348
unknown <muhammad.usman@funavry.com-at-890242958078>
2022-05-24 17:05:42 +0500

    updated header

--- apps/frontend/services/GetData.ts
+++ apps/frontend/services/GetData.ts
@@ -4,73 +4,68 @@ import { toast } from 'react-toastify';
 
 const notify = () => toast.success("Success Notification !", {
     position: toast.POSITION.TOP_RIGHT
-  });
+});
 
-  //const baseURL = 'http://157.90.67.186:3001/'
+//const baseURL = 'http://157.90.67.186:3001/'
 
-const GetData = async (url:string,params:any, method:string, displayMessage:boolean): Promise<any>=>
-{
+const GetData = async (url: string, params: any, method: string, displayMessage: boolean): Promise<any> => {
 
     const data = {
-       data:null,
-       error: null
+        data: null,
+        error: null
     }
 
     const client = axios.create({
         baseURL: "",
         headers: {
-            "Access-Control-Allow-Origin":"*",
+            "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,*/*;q=0.8",
+            "Accept-Encoding": "gzip, deflate",
+            "Accept-Language": "en-US,en;q=0.5",
+            "Connection": "keep-alive",
+            "Access-Control-Allow-Origin": "*",
             // "Accept":"*",
             crossDomain: false,
-            "HTTP_ORIGIN":"https://site-staging.cnbcarabia.com"
+            "HTTP_ORIGIN": "https://site-staging.cnbcarabia.com"
             //Authorization : `Bearer ${token}`
         },
-        data:params
+        data: params
     });
 
-    if(method === 'post')
-    {
-        await client.post(url,params).then((res) => {
-            data.data = res.data  
-             
-        }).catch( (error) => {
-             
-            if(error.response)
-            {
-                data.error = {message: error.response.data.message}
+    if (method === 'post') {
+        await client.post(url, params).then((res) => {
+            data.data = res.data
+
+        }).catch((error) => {
+
+            if (error.response) {
+                data.error = { message: error.response.data.message }
 
             }
-            else if (error.request)
-            {
-                data.error = {message: error.message}
+            else if (error.request) {
+                data.error = { message: error.message }
             }
-            else
-            {
-                data.error = {message: error.message}
+            else {
+                data.error = { message: error.message }
             }
         })
     }
-    else if(method === 'get')
-    {
+    else if (method === 'get') {
         displayMessage && notify()
         await client.get(url, {
             params: {
-              ...(Object.keys(params).length && {...params})
+                ...(Object.keys(params).length && { ...params })
             }
-          }).then((res) => {
-            data.data = res.data 
-        }).catch( (error) => {
-            if(error.response)
-            {
-                data.error = {message: error.response.data.message}
+        }).then((res) => {
+            data.data = res.data
+        }).catch((error) => {
+            if (error.response) {
+                data.error = { message: error.response.data.message }
             }
-            else if (error.request)
-            {
-                data.error = {message: "Please check your internet connection or consult technical team"}//error.message
+            else if (error.request) {
+                data.error = { message: "Please check your internet connection or consult technical team" }//error.message
             }
-            else
-            {
-                data.error = {message: "Please check your internet connection or consult technical team"} //error.message
+            else {
+                data.error = { message: "Please check your internet connection or consult technical team" } //error.message
             }
         })
     }
